5c82bd8df9af99bc28801fb86321dfcffaec1c79
Elbouchouki <elbouchoukigamer@gmail.com>
2023-05-24 23:56:03 +0100

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+## Projet JEE  Spring Angular - Digital Banking
+
+### Architecture du projet
+
+![Architecture](docs/architecture.png)
+
+### Diagramme de classe
+
+![Diagramme de classe](docs/Class.png)
+
---
+++ src/main/java/ma/elbouchouki/digitalbanking/DigitalBankingApplication.java
@@ -0,0 +1,11 @@
+package ma.elbouchouki.digitalbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingApplication.class, args);
+    }
+}
